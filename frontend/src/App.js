895b8606ad1ad5f3c2d79f5f9a24cba29baf618a
Rahul Prajapati <rahul159147@gmail.com>
2023-06-03 16:35:59 +0530

    frontend commits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Fragment>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
   )
 }
 
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import {Navbar, Nav, Container} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                    <Navbar.Brand>caRt 97</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ms-auto'>
+                            <LinkContainer to='/cart'>
+                            <Nav.Link><FaShoppingCart />Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                            <Nav.Link><FaUser />Sign in</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header;
