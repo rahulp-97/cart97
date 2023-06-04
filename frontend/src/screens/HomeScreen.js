3dc3260ee6fba6903c8303bd429928adac152410
Rahul Prajapati <rahul159147@gmail.com>
2023-06-04 18:58:28 +0530

    fetching all data using reduxjs toolkit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,47 @@
-import React,{Fragment, useState, useEffect} from 'react';
-import axios from 'axios';
-import {Row, Col} from 'react-bootstrap';
-import Product from '../components/Product';
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 // "proxy": "http://localhost:8000",
+//-- data --
+//The received data from the query.
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
+  // const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
-    const fetchProducts = async () => {
-      const {data} = await axios.get('http://localhost:8000/api/products');
-      // console.log(data);
-      setProducts(data);
-    }
-    fetchProducts();
-  }, [])
+  // useEffect(()=>{
+  //   const fetchProducts = async () => {
+  //     const {data} = await axios.get('http://localhost:8000/api/products');
+  //     // console.log(data);
+  //     setProducts(data);
+  //   }
+  //   fetchProducts();
+  // }, [])
   return (
     <Fragment>
-        <h1>Latest products</h1>
-        <Row>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : 
+        <Fragment>
+          <h1>Latest products</h1>
+          <Row>
             {products.map((product) => {
-                return (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                    </Col>
-                )
+              return (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              );
             })}
-        </Row>
+          </Row>
+        </Fragment>
+      }
     </Fragment>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,30 @@
+import { PRODUCTS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+//-- query
+//query can be a function that returns either a string or an object
+// which is passed to your baseQuery. If you are using fetchBaseQuery,
+// this can return either a string or an object of properties in FetchArgs.
+//-- keepUnusedDataFor
+// This is how long RTK Query will keep your data cached for after the last component unsubscribes.
+// For example, if you query an endpoint, then unmount the component,
+// then mount another component that makes the same request within the given time frame, the most recent value will be served from the cache.
+export const productsApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: PRODUCTS_URL
+      }),
+      keepUnusedDataFor: 5
+    }),
+    getProductDetails: builder.query({
+        query: (productId) => ({
+            url: `${PRODUCTS_URL}/${productId}`
+        }),
+        keepUnusedDataFor: 5
+    })
+  }),
+});
+
+// convention is to use at prefix and Query at the end of query name.
+export const {useGetProductsQuery, useGetProductDetailsQuery} = productsApiSlice;
