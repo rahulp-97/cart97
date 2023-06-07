29b281fe85608135527a31ae8a50f2a38ab38953
Rahul Prajapati <rahul159147@gmail.com>
2023-06-07 12:55:26 +0530

    logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../slices/usersApiSlice";
+import {logout} from '../slices/authSlice';
 import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const logoutHandler=()=>{
-    console.log('logout');
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try{
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      console.log('logged out')
+      navigate('/login');
+    } catch(error){
+      console.log(error);
+      toast.error(error?.data?.message || error.error);
+    }
   }
   return (
     <header>
